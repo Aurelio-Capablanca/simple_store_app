4184ab50c809146d5392744e224abfc191084064
Aurelio <aurelio.iempereur@gmail.com>
2025-11-09 13:57:01 -0600

    adding JWT
    all the thing will be made via JS now, so little to do on PHP for now

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,21 +3,38 @@
 namespace App\Http\Controllers;
 
 use App\Models\Store;
+use Firebase\JWT\JWT;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth as FacadesAuth;
 use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
     //UI retieval
-    public function index(){
+    public function index()
+    {
         $stores = Store::all();
         $users = DB::table('users', 'usr')->
             select('name', 'email', 'str.store_name', 'usr.id')
             ->leftJoin('store as str', 'str.id_store', '=', 'usr.id_store')
             ->get();
-        return view("product",compact("users","stores"));
+        $jwt = $this->generate_token();
+        return view("product", compact("users", "stores","jwt"));
     }
 
     //Logical operations
+
+    public function generate_token()
+    {
+        $user = Auth::user();
+        $payload = [
+            'sub' => $user->id,
+            'exp' => time() + 3600,
+        ];
+        return JWT::encode($payload, 'MGACAQAwEAYHKoZIzj0CAQYFK4EEACMESTBHAgEBBEIBGV7Ryk5PYpRLoFh+0f3KU7aS1bxmUy4QDTA7jfFyF6NkVF9mjXToZ9tSVFsPaOX79r86zZSwOihlAzQmEriPA4Q=', 'EC512');
+    }
+
+
 
 }
